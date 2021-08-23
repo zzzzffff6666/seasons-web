a83de56ccdab832ea1ea396a1f2685efc4000196
zhangfu07 <zhangfu07@meituan.com>
2021-08-23 11:25:10 +0800

    router test(failure)

---
+++ src/components/connect/login.vue
@@ -0,0 +1,22 @@
+<template>
+  <button v-for="item in items" :key="item" @click="menuClick">
+    {{ item }}
+  </button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: ["home", "register"],
+      collapsed: false,
+    };
+  },
+  methods: {
+    menuClick(e) {
+      // 路由跳转
+      this.$router.push(e.key);
+    },
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
-import 'element-plus/lib/theme-chalk/index.css';
+import 'element-plus/lib/theme-chalk/index.css'
+import axios from 'axios'
+import router from './router.js'
 
-createApp(App).use(ElementPlus).mount('#app')
+axios.default.baseURL = "http://localhost:8080/"
+
+createApp(App)
+.use(ElementPlus)
+.use(router)
+.mount('#app')
